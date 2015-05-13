2ba5c4f706029112aafe375c204a5320e92a9f21
Welton Rodrigo Torres Nascimento <rodrigo@familianascimento.org>
2015-05-13 17:31:00 -0400

    Activity Nova Despesa.

--- app/src/main/java/org/familianascimento/rodrigo/contasdacasa/NovaDespesa.java
+++ app/src/main/java/org/familianascimento/rodrigo/contasdacasa/NovaDespesa.java
@@ -1,23 +1,32 @@
 package org.familianascimento.rodrigo.contasdacasa;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.parse.ParseObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 
-public class NovaDespesa extends AppCompatActivity{
+public class NovaDespesa extends AppCompatActivity implements
+        DatePickerDialog.OnDateSetListener, View.OnFocusChangeListener {
 
     private EditText mNomeField;
     private EditText mValorField;
     private EditText mVencimentoField;
+
+    private DatePickerDialog mDatePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,8 @@ public class NovaDespesa extends AppCompatActivity{
         mNomeField = (EditText) findViewById(R.id.nomeEditText);
         mValorField = (EditText) findViewById(R.id.valorEditText);
         mVencimentoField = (EditText) findViewById(R.id.vencimentoEditText);
+
+        mVencimentoField.setOnFocusChangeListener(this);
 
     }
 
@@ -58,6 +69,40 @@ public class NovaDespesa extends AppCompatActivity{
     }
 
     @Override
+    public void onFocusChange (View v, boolean hasFocus) {
+        // Just to be sure
+        if (hasFocus) {
+
+            Calendar mCalendar = Calendar.getInstance();
+
+            mDatePickerDialog = new DatePickerDialog(
+                    getBaseContext(),
+                    this,
+                    mCalendar.get(Calendar.YEAR),
+                    mCalendar.get(Calendar.MONTH),
+                    mCalendar.get(Calendar.DAY_OF_MONTH)
+            );
+            mDatePickerDialog.show();
+        }
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+        Calendar mCalendar = Calendar.getInstance();
+
+        mCalendar.set(Calendar.YEAR, year);
+        mCalendar.set(Calendar.MONTH, monthOfYear);
+        mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        String myformat = "dd/MM/yy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(myformat, Locale.US);
+
+        mVencimentoField.setText(simpleDateFormat.format(mCalendar.getTime()));
+
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         int id = item.getItemId();
@@ -69,5 +114,12 @@ public class NovaDespesa extends AppCompatActivity{
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mDatePickerDialog = null;
     }
 }
